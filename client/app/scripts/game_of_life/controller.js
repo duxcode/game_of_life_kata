97d3aab755f0dc3a944b486fce63e7901eaa42aa
Fernando Portilla <fernandoportilla1@gmail.com>
2016-07-25 16:55:31 -0500

    first implementation of test in katana

--- client/app/scripts/game_of_life/controller.js
+++ client/app/scripts/game_of_life/controller.js
@@ -4,6 +4,85 @@ angular.module('Game_of_life')
 .controller('game_of_life', function ($scope) {
 
   $scope.controller_loaded = 'Game of life loaded!';
+  $scope.kata = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+  ];
+
+  $scope.buildKata = function() {
+    angular.forEach($scope.kata, function(value, line) {
+      angular.forEach(value, function(item, column) {
+        if(line === 1 && column === 4) {
+          $scope.kata[line][column] = 1;
+        }
+
+        if(line === 2 && (column === 3 || column === 4)) {
+          $scope.kata[line][column] = 1;
+        }
+
+      });
+    });
+
+    return $scope.kata;
+  };
+
+  function getNeighboars(lineSearch, columnSearch, cell) {
+    var minLine = lineSearch;
+    if(lineSearch !== 0) {
+      minLine--;
+    }
+    var maxLine = parseInt(lineSearch) + 1;
+    
+    var maxColumn = parseInt(columnSearch) + 1;
+    var minColumn = parseInt(columnSearch);
+    if(parseInt(columnSearch) !== 0) {
+      minColumn = parseInt(columnSearch) - 1;
+    }
+
+    var live = 0;
+    var count = 0;
+    
+    angular.forEach($scope.kata, function(value, line) {
+      angular.forEach(value, function(item, column) {
+        if(line <= maxLine && line >= minLine) {
+          if(item === 1) {
+            if(maxColumn === column || minColumn === column || column === columnSearch) {
+              count = count + 1;
+            }
+          }
+        }
+      });
+    });
+    
+    if(cell === 0) {
+      if(count === 3) {
+        live = 1;
+      }
+    } else {
+      if(count <= 3 || count >= 2) {
+        live = 1;
+      }
+    }
+
+    return live;
+  }
+
+  $scope.startIteration = function() {
+    $scope.buildKata();
+    var kataCopy = angular.copy($scope.kata);
+
+    angular.forEach(kataCopy, function(value, line) {
+      angular.forEach(value, function(item, column) {
+        var status = getNeighboars(line, column, item);
+        kataCopy[line][column] = status;
+      });
+    });
+
+    return kataCopy; 
+  };
+
 
 })
 .config(function ($routeProvider) {
--- client/app/scripts/game_of_life/specs/controller_spec.js
+++ client/app/scripts/game_of_life/specs/controller_spec.js
@@ -16,6 +16,104 @@ describe('Controller: game_of_life', function () {
     it('should set "controller_loaded" variable in scope', function() {
       expect(scope.controller_loaded).toContain('loaded');
     });
+
+    it('should init to array kata', function() {
+      expect(scope.kata.length).toEqual(4);
+      
+      expect(scope.kata[0][0]).toEqual(0);
+      expect(scope.kata[0][1]).toEqual(0);
+      expect(scope.kata[0][2]).toEqual(0);
+      expect(scope.kata[0][3]).toEqual(0);
+      expect(scope.kata[0][4]).toEqual(0);
+      expect(scope.kata[0][5]).toEqual(0);
+      expect(scope.kata[0][6]).toEqual(0);
+      expect(scope.kata[0][7]).toEqual(0);
+
+      expect(scope.kata[1][0]).toEqual(0);
+      expect(scope.kata[1][1]).toEqual(0);
+      expect(scope.kata[1][2]).toEqual(0);
+      expect(scope.kata[1][3]).toEqual(0);
+      expect(scope.kata[1][4]).toEqual(0);
+      expect(scope.kata[1][5]).toEqual(0);
+      expect(scope.kata[1][6]).toEqual(0);
+      expect(scope.kata[1][7]).toEqual(0);
+
+      expect(scope.kata[2][0]).toEqual(0);
+      expect(scope.kata[2][1]).toEqual(0);
+      expect(scope.kata[2][2]).toEqual(0);
+      expect(scope.kata[2][3]).toEqual(0);
+      expect(scope.kata[2][4]).toEqual(0);
+      expect(scope.kata[2][5]).toEqual(0);
+      expect(scope.kata[2][6]).toEqual(0);
+      expect(scope.kata[2][7]).toEqual(0);
+
+      expect(scope.kata[3][0]).toEqual(0);
+      expect(scope.kata[3][1]).toEqual(0);
+      expect(scope.kata[3][2]).toEqual(0);
+      expect(scope.kata[3][3]).toEqual(0);
+      expect(scope.kata[3][4]).toEqual(0);
+      expect(scope.kata[3][5]).toEqual(0);
+      expect(scope.kata[3][6]).toEqual(0);
+      expect(scope.kata[3][7]).toEqual(0);
+    });
+
+    it('should change to status array kata', function() {
+      expect(scope.buildKata()[0].length).toEqual(8);
+      expect(scope.buildKata()[1].length).toEqual(8);
+      expect(scope.buildKata()[2].length).toEqual(8);
+      expect(scope.buildKata()[3].length).toEqual(8);
+
+      expect(scope.buildKata().length).toEqual(4);
+
+      expect(scope.buildKata()[0][0]).toEqual(0);
+      expect(scope.buildKata()[0][1]).toEqual(0);
+      expect(scope.buildKata()[0][2]).toEqual(0);
+      expect(scope.buildKata()[0][3]).toEqual(0);
+      expect(scope.buildKata()[0][4]).toEqual(0);
+      expect(scope.buildKata()[0][5]).toEqual(0);
+      expect(scope.buildKata()[0][6]).toEqual(0);
+      expect(scope.buildKata()[0][7]).toEqual(0);
+
+      expect(scope.buildKata()[1][0]).toEqual(0);
+      expect(scope.buildKata()[1][1]).toEqual(0);
+      expect(scope.buildKata()[1][2]).toEqual(0);
+      expect(scope.buildKata()[1][3]).toEqual(0);
+      expect(scope.buildKata()[1][4]).toEqual(1);
+      expect(scope.buildKata()[1][5]).toEqual(0);
+      expect(scope.buildKata()[1][6]).toEqual(0);
+      expect(scope.buildKata()[1][7]).toEqual(0);
+
+      expect(scope.buildKata()[2][0]).toEqual(0);
+      expect(scope.buildKata()[2][1]).toEqual(0);
+      expect(scope.buildKata()[2][2]).toEqual(0);
+      expect(scope.buildKata()[2][3]).toEqual(1);
+      expect(scope.buildKata()[2][4]).toEqual(1);
+      expect(scope.buildKata()[2][5]).toEqual(0);
+      expect(scope.buildKata()[2][6]).toEqual(0);
+      expect(scope.buildKata()[2][7]).toEqual(0);
+
+      expect(scope.buildKata()[3][0]).toEqual(0);
+      expect(scope.buildKata()[3][1]).toEqual(0);
+      expect(scope.buildKata()[3][2]).toEqual(0);
+      expect(scope.buildKata()[3][3]).toEqual(0);
+      expect(scope.buildKata()[3][4]).toEqual(0);
+      expect(scope.buildKata()[3][5]).toEqual(0);
+      expect(scope.buildKata()[3][6]).toEqual(0);
+      expect(scope.buildKata()[3][7]).toEqual(0);
+    });
+
+    it('should start iteration to kata', function() {
+      
+      var kata = [
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,1,1,0,0,0],
+        [0,0,0,1,1,0,0,0],
+        [0,0,0,0,0,0,0,0]
+      ];
+
+      expect(scope.startIteration()).toEqual(kata);
+    });
+
   });
 
   describe('when going to /game_of_life', function() {
